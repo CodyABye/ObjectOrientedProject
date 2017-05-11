6907339b5cabf99c599780be2b0488cea2eaa6a9
Mr_Bye <cabrocks888@gmail.com>
2017-05-11 11:30:02 -0500

    final commit of day

--- Group_Project/Group_Project/Form1.Designer.cs
+++ Group_Project/Group_Project/Form1.Designer.cs
@@ -34,22 +34,22 @@
             this.btnServerAdd = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.grpParties = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstWaitList = new System.Windows.Forms.ListBox();
+            this.txtpSize = new System.Windows.Forms.TextBox();
+            this.txtpName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.btnPartiesAssign = new System.Windows.Forms.Button();
             this.btnPartiesAdd = new System.Windows.Forms.Button();
             this.grpTables = new System.Windows.Forms.GroupBox();
             this.listBox4 = new System.Windows.Forms.ListBox();
             this.listBox3 = new System.Windows.Forms.ListBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtpName = new System.Windows.Forms.TextBox();
-            this.txtpSize = new System.Windows.Forms.TextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.lstWaitList = new System.Windows.Forms.ListBox();
             this.grpServers.SuspendLayout();
             this.grpParties.SuspendLayout();
-            this.grpTables.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.grpTables.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpServers
@@ -58,9 +58,9 @@
             this.grpServers.Controls.Add(this.btnServerAssign);
             this.grpServers.Controls.Add(this.btnServerAdd);
             this.grpServers.Location = new System.Drawing.Point(1019, 80);
-            this.grpServers.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpServers.Margin = new System.Windows.Forms.Padding(4);
             this.grpServers.Name = "grpServers";
-            this.grpServers.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpServers.Padding = new System.Windows.Forms.Padding(4);
             this.grpServers.Size = new System.Drawing.Size(360, 304);
             this.grpServers.TabIndex = 0;
             this.grpServers.TabStop = false;
@@ -72,7 +72,7 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
             this.listBox1.Location = new System.Drawing.Point(56, 48);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(159, 116);
             this.listBox1.TabIndex = 2;
@@ -81,7 +81,7 @@
             // btnServerAssign
             // 
             this.btnServerAssign.Location = new System.Drawing.Point(179, 268);
-            this.btnServerAssign.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnServerAssign.Margin = new System.Windows.Forms.Padding(4);
             this.btnServerAssign.Name = "btnServerAssign";
             this.btnServerAssign.Size = new System.Drawing.Size(100, 28);
             this.btnServerAssign.TabIndex = 1;
@@ -91,7 +91,7 @@
             // btnServerAdd
             // 
             this.btnServerAdd.Location = new System.Drawing.Point(69, 268);
-            this.btnServerAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnServerAdd.Margin = new System.Windows.Forms.Padding(4);
             this.btnServerAdd.Name = "btnServerAdd";
             this.btnServerAdd.Size = new System.Drawing.Size(100, 28);
             this.btnServerAdd.TabIndex = 0;
@@ -120,114 +120,13 @@
             this.grpParties.Controls.Add(this.btnPartiesAssign);
             this.grpParties.Controls.Add(this.btnPartiesAdd);
             this.grpParties.Location = new System.Drawing.Point(1019, 384);
-            this.grpParties.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpParties.Margin = new System.Windows.Forms.Padding(4);
             this.grpParties.Name = "grpParties";
-            this.grpParties.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpParties.Padding = new System.Windows.Forms.Padding(4);
             this.grpParties.Size = new System.Drawing.Size(360, 304);
             this.grpParties.TabIndex = 2;
             this.grpParties.TabStop = false;
             this.grpParties.Text = "Parties";
-            // 
-            // btnPartiesAssign
-            // 
-            this.btnPartiesAssign.Location = new System.Drawing.Point(147, 79);
-            this.btnPartiesAssign.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.btnPartiesAssign.Name = "btnPartiesAssign";
-            this.btnPartiesAssign.Size = new System.Drawing.Size(100, 28);
-            this.btnPartiesAssign.TabIndex = 1;
-            this.btnPartiesAssign.Text = "Assign";
-            this.btnPartiesAssign.UseVisualStyleBackColor = true;
-            // 
-            // btnPartiesAdd
-            // 
-            this.btnPartiesAdd.Location = new System.Drawing.Point(14, 79);
-            this.btnPartiesAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.btnPartiesAdd.Name = "btnPartiesAdd";
-            this.btnPartiesAdd.Size = new System.Drawing.Size(100, 28);
-            this.btnPartiesAdd.TabIndex = 0;
-            this.btnPartiesAdd.Text = "Add Party";
-            this.btnPartiesAdd.UseVisualStyleBackColor = true;
-            this.btnPartiesAdd.Click += new System.EventHandler(this.BtnPartiesAdd_Click);
-            // 
-            // grpTables
-            // 
-            this.grpTables.Controls.Add(this.listBox4);
-            this.grpTables.Controls.Add(this.listBox3);
-            this.grpTables.Controls.Add(this.listBox2);
-            this.grpTables.Location = new System.Drawing.Point(37, 80);
-            this.grpTables.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.grpTables.Name = "grpTables";
-            this.grpTables.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.grpTables.Size = new System.Drawing.Size(956, 608);
-            this.grpTables.TabIndex = 3;
-            this.grpTables.TabStop = false;
-            this.grpTables.Text = "Tables";
-            // 
-            // listBox4
-            // 
-            this.listBox4.AllowDrop = true;
-            this.listBox4.FormattingEnabled = true;
-            this.listBox4.ItemHeight = 16;
-            this.listBox4.Location = new System.Drawing.Point(8, 23);
-            this.listBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.listBox4.Name = "listBox4";
-            this.listBox4.Size = new System.Drawing.Size(159, 116);
-            this.listBox4.TabIndex = 2;
-            // 
-            // listBox3
-            // 
-            this.listBox3.AllowDrop = true;
-            this.listBox3.FormattingEnabled = true;
-            this.listBox3.ItemHeight = 16;
-            this.listBox3.Location = new System.Drawing.Point(8, 148);
-            this.listBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.listBox3.Name = "listBox3";
-            this.listBox3.Size = new System.Drawing.Size(159, 116);
-            this.listBox3.TabIndex = 1;
-            // 
-            // listBox2
-            // 
-            this.listBox2.AllowDrop = true;
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 16;
-            this.listBox2.Location = new System.Drawing.Point(176, 23);
-            this.listBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(159, 116);
-            this.listBox2.TabIndex = 0;
-            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 23);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 17);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Party Name";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(11, 56);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 17);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Party Size";
-            // 
-            // txtpName
-            // 
-            this.txtpName.Location = new System.Drawing.Point(116, 23);
-            this.txtpName.Name = "txtpName";
-            this.txtpName.Size = new System.Drawing.Size(100, 22);
-            this.txtpName.TabIndex = 4;
-            // 
-            // txtpSize
-            // 
-            this.txtpSize.Location = new System.Drawing.Point(116, 50);
-            this.txtpSize.Name = "txtpSize";
-            this.txtpSize.Size = new System.Drawing.Size(100, 22);
-            this.txtpSize.TabIndex = 5;
             // 
             // groupBox1
             // 
@@ -248,6 +147,107 @@
             this.lstWaitList.Size = new System.Drawing.Size(203, 132);
             this.lstWaitList.TabIndex = 0;
             // 
+            // txtpSize
+            // 
+            this.txtpSize.Location = new System.Drawing.Point(116, 50);
+            this.txtpSize.Name = "txtpSize";
+            this.txtpSize.Size = new System.Drawing.Size(100, 22);
+            this.txtpSize.TabIndex = 5;
+            // 
+            // txtpName
+            // 
+            this.txtpName.Location = new System.Drawing.Point(116, 23);
+            this.txtpName.Name = "txtpName";
+            this.txtpName.Size = new System.Drawing.Size(100, 22);
+            this.txtpName.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Party Size";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Party Name";
+            // 
+            // btnPartiesAssign
+            // 
+            this.btnPartiesAssign.Location = new System.Drawing.Point(147, 79);
+            this.btnPartiesAssign.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPartiesAssign.Name = "btnPartiesAssign";
+            this.btnPartiesAssign.Size = new System.Drawing.Size(100, 28);
+            this.btnPartiesAssign.TabIndex = 1;
+            this.btnPartiesAssign.Text = "Assign";
+            this.btnPartiesAssign.UseVisualStyleBackColor = true;
+            // 
+            // btnPartiesAdd
+            // 
+            this.btnPartiesAdd.Location = new System.Drawing.Point(14, 79);
+            this.btnPartiesAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPartiesAdd.Name = "btnPartiesAdd";
+            this.btnPartiesAdd.Size = new System.Drawing.Size(100, 28);
+            this.btnPartiesAdd.TabIndex = 0;
+            this.btnPartiesAdd.Text = "Add Party";
+            this.btnPartiesAdd.UseVisualStyleBackColor = true;
+            this.btnPartiesAdd.Click += new System.EventHandler(this.BtnPartiesAdd_Click);
+            // 
+            // grpTables
+            // 
+            this.grpTables.Controls.Add(this.listBox4);
+            this.grpTables.Controls.Add(this.listBox3);
+            this.grpTables.Controls.Add(this.listBox2);
+            this.grpTables.Location = new System.Drawing.Point(37, 86);
+            this.grpTables.Margin = new System.Windows.Forms.Padding(4);
+            this.grpTables.Name = "grpTables";
+            this.grpTables.Padding = new System.Windows.Forms.Padding(4);
+            this.grpTables.Size = new System.Drawing.Size(956, 608);
+            this.grpTables.TabIndex = 3;
+            this.grpTables.TabStop = false;
+            this.grpTables.Text = "Tables";
+            // 
+            // listBox4
+            // 
+            this.listBox4.AllowDrop = true;
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 16;
+            this.listBox4.Location = new System.Drawing.Point(8, 23);
+            this.listBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(159, 116);
+            this.listBox4.TabIndex = 2;
+            // 
+            // listBox3
+            // 
+            this.listBox3.AllowDrop = true;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(8, 148);
+            this.listBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(159, 116);
+            this.listBox3.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.AllowDrop = true;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(176, 23);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(159, 116);
+            this.listBox2.TabIndex = 0;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -257,15 +257,15 @@
             this.Controls.Add(this.grpParties);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.grpServers);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Restaurants";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.grpServers.ResumeLayout(false);
             this.grpParties.ResumeLayout(false);
             this.grpParties.PerformLayout();
-            this.grpTables.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
+            this.grpTables.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
