a0dccaa40ac8b30067c51703f47aa5388584cfde
Mr_Bye <cabrocks888@gmail.com>
2017-05-15 22:07:38 -0500

    Added Server object RestServer

---
+++ Group_Project/Group_Project/RestServer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Project
+{
+    class RestServer
+    {
+        public int ServID { get; set; }
+        public string ServName { get; set; }
+
+        public RestServer()
+        {
+
+        }
+        public RestServer(int theID, string theName)
+        {
+            this.ServID = theID;
+            this.ServName = theName;
+        }
+        public override string ToString()
+        {
+            return ServName;
+        }
+    }
+}
