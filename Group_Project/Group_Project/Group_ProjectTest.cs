a203d8ce91acc111feddbdf47d3ab5032dedbbc9
Connor <connor.g.mayhew@gmail.com>
2017-05-23 11:13:48 -0500

    Corrected testing for CustomerReport class

--- Group_Project/Group_Project/Group_ProjectTest.cs
+++ Group_Project/Group_Project/Group_ProjectTest.cs
@@ -10,11 +10,60 @@ namespace Group_Project
         
         public void Init()
         {
+            
+        }
+
+        //Tests for CustomerReportClass
+        //PartySize Setter Validation Test
+        [Test]
+        public void MakeCustomerReport_WithValidatedPartySize()
+        {
             int testCustomerTotal;
+            bool isCorrectPartySize = false;
+
             CustomerReport testCustomerReport = new CustomerReport();
+
             testCustomerReport.AddToCustomerTotal(3);
-            testCustomerReport.ToString();
             testCustomerTotal = testCustomerReport.CustomerTotal;
+
+            if (testCustomerTotal == 3)
+            {
+                isCorrectPartySize = true;
+            }
+            else
+            {
+                isCorrectPartySize = false;
+            }
+            
+
+            Assert.IsTrue(isCorrectPartySize);
+        }
+
+        //ToString Validation Test
+        [Test]
+        public void MakeCustomerReport_WithValidatedToString()
+        {
+            string testCustomerString;
+            string testCustomerString2;
+            bool isCorrectString = false;
+
+            CustomerReport testCustomerReport = new CustomerReport();
+
+            testCustomerReport.AddToCustomerTotal(3);
+            testCustomerString = testCustomerReport.ToString();
+            testCustomerString2 = "Report: Total Customers: " + testCustomerReport.CustomerTotal.ToString();
+
+            if (testCustomerString == testCustomerString2)
+            {
+                isCorrectString = true;
+            }
+            else
+            {
+                isCorrectString = false;
+            }
+
+
+            Assert.IsTrue(isCorrectString);
         }
     }
 }
